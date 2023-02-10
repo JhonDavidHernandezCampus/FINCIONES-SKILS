a28fc558bbd4a804e157a46a8d174cc21d482a54
JhonHernandez_Campus <jhherdaferrer@gmail.com>
2023-02-10 13:36:55 -0500

    Parte 1

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+def progamaMenu():
+    menu=input(
+        """
+        --------------------El Menu-------------------
+        1.   CREAR GRUPO ARTEMIS:
+        1.1  LISTAR CAMPERS  DE ARTEMIS:
+        1.2  AGREGAR CAMPERS A ARTEMIS:
+        1.3  ELIMINAR CAMPERS DE ARTEMIS:
+        1.4  ORDENAR ALFABETICAMENTE EN LA LISTA DE ARTEMIS:
+        1.5  BUSCAR CAMPERS EN LA LISTA DE ARTEMIS:
+        2.   CREAR GRUPO SPUTNIK:
+        2.1  LISTAR CAMPERS DE SPUTNIT
+        2.2 AGREGAR CAMPERS A SPU"""
+        
+    )
