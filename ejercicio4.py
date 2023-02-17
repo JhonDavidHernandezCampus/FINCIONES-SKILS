5f1a973fa087525efb06723f2b67d0af731ad6e7
JhonHernandez_Campus <jhherdaferrer@gmail.com>
2023-02-17 13:28:20 -0500

    parte final

--- ejercicio4.py
+++ ejercicio4.py
@@ -22,12 +22,36 @@ for i in range(can):
         print(veDic)
         datos.append(veDic)
 
-
-print(json.dumps(datos,indent=4) )
+suma=0
+posicion=[]
+print(json.dumps(datos,indent=4))
+con=0
 for i in datos:
-    print(i["Cantidad"])
+    art=(i["Cantidad"])
+    suma=suma + art
+    con+=1
+    if con == 5:
+        posicion.append(suma)
+        con=0
+        suma=0
+
+ 
+def mayor(posicion):
+    max = posicion[0]
+    for x in posicion:
+        if x > max:
+            max = x
+    return max 
+a=mayor(posicion)
+don=posicion.index(a)
+print(don)
+print(datos[don**5])
 
 
+
+
+print(suma)
+print(posicion)
 
 
 
