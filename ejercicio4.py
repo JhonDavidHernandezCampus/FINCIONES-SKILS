6d3cbdad2c23a5f8f36ead8f648dd0e4531c4047
JhonHernandez_Campus <jhherdaferrer@gmail.com>
2023-02-17 12:25:18 -0500

    ejercicio 4.1

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,4 +1,4 @@
-
+import json
 """ 4. Una empresa tiene 500 almacenes. Cada almacén debe
 reportar el nombre y 5 registros c/u, contiene el tipo de articulo
 y el número de unidades vendidas de ese artículo.
@@ -7,28 +7,27 @@ Haga un programa en Python para determinar cuál fue el
 almacén que mas vendió, cual fue el articulo más vendido de
 ese almacén y cual el más vendido en general. """
 
-lista=[]
-matriz=[]
-print(matriz)
 
-def pedirArt():
+
+datos=[]
+
+can=int(input("cuantos almacenes son: "))
+for i in range(can):
+    print("\n -------------NUEVO ALMACEN--------------")
     nomAlma=input('Nombre del almacen: ')
     for i in range(5):
         tpArt=input(f'Tipo de articulo: ')
         canVen=int(input('Cantidad de unidades vendias: '))
-        lista.append(nomAlma)
-        lista.append(tpArt) 
-        lista.append(canVen)
-        matriz.append(lista)
-        print(f'{lista}')
-        print(matriz)
-        lista.clear()
-        print(f'{lista}')
-    return matriz
+        veDic={'nombre':nomAlma,'Tipo':tpArt,'Cantidad':canVen }
+        print(veDic)
+        datos.append(veDic)
 
-Can= int(input(f"Cuantos almacenes son: "))
-i=0
-while Can>i:
-    i+=1
-    pedirArt()
-    print(matriz)
+
+print(json.dumps(datos,indent=4) )
+for i in datos:
+    print(i["Cantidad"])
+
+
+
+
+
